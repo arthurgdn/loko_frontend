494b67f1a05a3aa1ce41ff0b2e826bb4b6215f44
Arthur Guédon <arthur.guedon@student-cs.fr>
2020-06-22 16:01:28 +0200

    resoud le problème d'invite de membres

--- src/components/groups/ManageGroup.js
+++ src/components/groups/ManageGroup.js
@@ -41,12 +41,14 @@ const ManageGroup = ({collaborators,startSetCollaborators,group,history}) =>{
         if((collaborators.length>0 && Object.keys(collaborators[0]).length===3)||collaborators.length===0){
             const formattedCollaborators = []
             for (const collaborator of collaborators){
-                formattedCollaborators.push({collaborator : collaborator.collaborator,value:collaborator.collaborator,label:collaborator.firstName + ' '+collaborator.lastName})
+                if (!members.find((member)=>member.user===collaborator.collaborator)){
+                    formattedCollaborators.push({collaborator : collaborator.collaborator,value:collaborator.collaborator,label:collaborator.firstName + ' '+collaborator.lastName})
+                }
             }
             setDisplayedCollaborators(formattedCollaborators)
         }
-    },[collaborators,startSetCollaborators])
-
+    },[members,setMembers,collaborators,startSetCollaborators])
+    
     const onInviteMemberChange= (member)=>{
         setInvite(member)
     }
