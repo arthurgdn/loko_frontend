6961a7db16a4433c517afcbd4b35fb28f01123ab
Arthur Guédon <arthur.guedon@student-cs.fr>
2021-06-19 19:05:14 +0200

    lint settings components

--- src/components/settings/EditProfilePage.js
+++ src/components/settings/EditProfilePage.js
@@ -1,19 +1,19 @@
-import React,{useEffect} from 'react'
+import React from 'react';
 
-import EditProfileForm from './EditProfileForm'
+import EditProfileForm from './EditProfileForm';
 
 export default ()=>{
-    
-    return (
-        <div>
-            <div className="page-header">
-                <div className="content-container">
-                    <h1 className="page-header__title">Modifier le profil</h1>
-                </div>
-            </div>
-            <div className="content-container">
-                <EditProfileForm/>
-            </div>
+
+  return (
+    <div>
+      <div className="page-header">
+        <div className="content-container">
+          <h1 className="page-header__title">Modifier le profil</h1>
         </div>
-    )
-}
+      </div>
+      <div className="content-container">
+        <EditProfileForm/>
+      </div>
+    </div>
+  );
+};
