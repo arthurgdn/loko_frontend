9b9ecef6edc2967ec28e87c91854aea7d237ed16
Arthur Guédon <arthur.guedon@student-cs.fr>
2021-06-19 18:56:34 +0200

    lint search components

--- src/components/search/SearchedOffers.js
+++ src/components/search/SearchedOffers.js
@@ -1,41 +1,45 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import moment from 'moment'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import moment from 'moment';
 export default ({offers})=>{
-    const completedStatusMapper=[{value:"created",label:"En attente de réponse"},
-    {value:"inProgress",label:"Travail en cours"},
-    {value:"completed",label:"Annonce terminée"}]
+  const completedStatusMapper=[{value: 'created', label: 'En attente de réponse'},
+    {value: 'inProgress', label: 'Travail en cours'},
+    {value: 'completed', label: 'Annonce terminée'}];
 
-    const getStatus = (completedStatus)=>{
-        return completedStatusMapper.find((mapStatus)=>mapStatus.value===completedStatus)
-    }
-     
-    return (
-        <div className="content-container">
-        <div className="manager__miniature-list">
-            {offers.length>0?offers.map((offer)=>(
-                <div className="manager__miniature-wrapper">
-                    <Link to={'/offer/'+offer._id} style={{textDecoration:'none'}}>
-                        <div className="manager__title-wrapper">
-                            
-                            <h3>{offer.title}</h3>
-                            
-                        </div>
-                        <div className="manager__details-wrapper">
-                            <p>{offer.description.length>250?offer.description.slice(0,250)+'...':offer.description}</p>
-                            <div className="manager__bottom-container">
-                                <p>{getStatus(offer.completedStatus).label}</p>
-                                <span>Publié {moment(offer.createdAt).lang('fr').fromNow()}</span>
-                            </div>
-                        </div>
-                    </Link>
-                    
-                    
+  const getStatus = (completedStatus)=>{
+    return completedStatusMapper.find((mapStatus)=>mapStatus.value===completedStatus);
+  };
+
+  return (
+    <div className="content-container">
+      <div className="manager__miniature-list">
+        {offers.length>0?offers.map((offer)=>
+          <div
+            className="manager__miniature-wrapper"
+            key={offer._id}
+          >
+            <Link to={'/offer/'+offer._id} style={{textDecoration: 'none'}}>
+              <div className="manager__title-wrapper">
+
+                <h3>{offer.title}</h3>
+
+              </div>
+              <div className="manager__details-wrapper">
+                <p>{offer.description.length>250
+                  ?offer.description.slice(0, 250)+'...':offer.description}</p>
+                <div className="manager__bottom-container">
+                  <p>{getStatus(offer.completedStatus).label}</p>
+                  <span>Publié {moment(offer.createdAt).lang('fr').fromNow()}</span>
                 </div>
-            )):(<p className="search__infotext">Aucune annonce ne correspond à votre recherche</p>)}
-        </div>
-        
-        
-        </div>
-        )
-}
+              </div>
+            </Link>
+
+
+          </div>
+        ):<p className="search__infotext">Aucune annonce ne correspond à votre recherche</p>}
+      </div>
+
+
+    </div>
+  );
+};
--- src/components/search/SearchedUsers.js
+++ src/components/search/SearchedUsers.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
 export default ({users})=>{
-    return (
-        <div className="content-container">
-            {users.length>0 ?(
-                users.map((user)=>(
-                    <div key={user._id} className="collaborator__list">
-                            <Link to={'/profile/'+user._id} className="offer-element__comment-subheader">
-                                <img className="header__picture offer-element__comment-picture" src={process.env.DEV_URL+"/users/"+user._id+"/avatar"}/>
-                                <p>{user.firstName} {user.lastName}</p>
-                            </Link>
-                            
-                        </div>
-                    
-                ))
-            ):(<p className="search__infotext" >Aucun utilisateur ne correspond à votre recherche</p>)}
-        </div>
+  return (
+    <div className="content-container">
+      {users.length>0
+        ? users.map((user)=>
+          <div key={user._id} className="collaborator__list">
+            <Link to={'/profile/'+user._id} className="offer-element__comment-subheader">
+              <img
+                className="header__picture offer-element__comment-picture"
+                src={process.env.DEV_URL+'/users/'+user._id+'/avatar'}/>
+              <p>{user.firstName} {user.lastName}</p>
+            </Link>
+
+          </div>
+
         )
-}
+        :<p className="search__infotext" >Aucun utilisateur ne correspond à votre recherche</p>}
+    </div>
+  );
+};
